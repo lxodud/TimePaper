53c255346f78998b3efafc37c558d4393923865e
yejin <yejinoh58@gmail.com>
2025-02-24 00:56:20 +0900

    [SSD-41] feat: Timepaper 엔티티 생성

---
+++ backend/src/main/java/com/timepaper/backend/domain/timepaper/entity/Timepaper.java
@@ -0,0 +1,40 @@
+package com.timepaper.backend.domain.timepaper.entity;
+
+import com.timepaper.backend.domain.user.entity.User;
+import com.timepaper.backend.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "timepapers")
+public class Timepaper extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "creator_id", nullable = false)
+  private User creator;
+
+  @Column(nullable = false)
+  private String title;
+
+  private String recipientEmail;
+
+  private LocalDateTime releaseDate;
+
+}
