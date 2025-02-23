dbdf4974f0036d33861bea0d8e30932a5fdb3887
lxodud <xodud1998223@naver.com>
2025-02-23 20:08:41 +0900

    [SSD-54] style: static image path 모듈화

---
+++ frontend/src/constant/staticImagePath.js
@@ -0,0 +1,11 @@
+import timepaperLogo from '../assets/images/timepaperLogo.svg'
+import postitAfternoon from '../assets/images/postitAfternoon.svg'
+import postitNight from '../assets/images/postitNight.svg'
+import notFound from '../assets/images/notFound.svg'
+
+export default {
+  timepaperLogo,
+  postitAfternoon,
+  postitNight,
+  notFound
+}
