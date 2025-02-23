64ceaebf9b0f12d8c7da5a443a282856a8a534d4
lxodud <xodud1998223@naver.com>
2025-02-23 20:52:44 +0900

    [SSD-54] chore: icon 추가 및 path 모듈화

--- frontend/src/constant/staticImagePath.js
+++ frontend/src/constant/staticImagePath.js
@@ -2,10 +2,16 @@ import timepaperLogo from '../assets/images/timepaperLogo.svg'
 import postitAfternoon from '../assets/images/postitAfternoon.svg'
 import postitNight from '../assets/images/postitNight.svg'
 import notFound from '../assets/images/notFound.svg'
+import dropDownIcon from '../assets/images/dropDownIcon.svg'
+import homeIcon from '../assets/images/homeIcon.svg'
+import imageSelectIcon from '../assets/images/imageSelectIcon.svg'
 
 export default {
   timepaperLogo,
   postitAfternoon,
   postitNight,
-  notFound
+  notFound,
+  dropDownIcon,
+  homeIcon,
+  imageSelectIcon
 }
