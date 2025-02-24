5db1bd8eb00d26013c269d5e76dda25148e87bf1
yejin <yejinoh58@gmail.com>
2025-02-25 02:15:02 +0900

    [SSD-70] feat: User 엔티티에 UserDetails 구현하여 인증 및 권한 관리 기능 추가

--- backend/src/main/java/com/timepaper/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/timepaper/backend/domain/user/entity/User.java
@@ -3,14 +3,21 @@ package com.timepaper.backend.domain.user.entity;
 import com.timepaper.backend.global.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.util.Collection;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,7 +25,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users",
     uniqueConstraints = @UniqueConstraint(columnNames = "email")
 )
-public class User extends BaseTimeEntity {
+public class User extends BaseTimeEntity implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,7 +37,37 @@ public class User extends BaseTimeEntity {
   @Column(nullable = false)
   private String password;
 
+  @Enumerated(EnumType.STRING)
   private ROLE role;
 
 
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public String getUsername() {
+    return this.email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
