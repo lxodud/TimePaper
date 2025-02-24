872dbf2e7ab2a9b4718c6abbe2521f83ae672f34
daejungyoon <dbseowjd12@gmail.com>
2025-02-24 19:40:11 +0900

    [SSD-67] refactor: 파일 이동으로 인한 삭제 반영
    - 파일 이동한 것을 반영하기 위한 커밋

--- frontend/src/components/createbutton/CreateButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import style from './CreateButton.module.css';
-export default function Button({ children }) {
-  return (
-    <button className={`${style.buttonBasic}`}>
-      {children}
-    </button>
-  );
-}
--- frontend/src/pages/TimePaperCreate.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function TimePaperCreate() {
-  return <div>TimePaperCreate</div>;
-}
