42204a5a39320db971b542307e1ab4ff5e2f225b
yejin <yejinoh58@gmail.com>
2025-02-24 15:46:43 +0900

    [SSD-65] feat: [WIP] 404 에러 페이지 구현
    - 에러 페이지 코드 작성 중

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import images from '../constant/staticImagePath';
+import { Link } from 'react-router-dom';
+import '../css/index.css';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div className="image-container">
+        <img src={images.notFound} alt="404 이미지" />
+      </div>
+      <div className="home-link">
+        <Link to="/">홈으로 이동</Link>
+      </div>
+    </>
+  );
 }
