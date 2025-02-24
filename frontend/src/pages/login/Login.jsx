a0bbf96a100686167f95865c16141507ec405f25
lxodud <xodud1998223@naver.com>
2025-02-24 15:37:21 +0900

    [SSD-62] feat: Login page css 구현

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import staticImagePath from '../../constant/staticImagePath';
+import styles from './Login.module.css'
 
 export default function Login() {
   return (
     <>
-      <div>
-        <img src={staticImagePath.timepaperLogo} />
-        <form action="">
-          <input type="text" placeholder="아이디(이메일)" />
-          <br />
-          <input type="text" placeholder="비밀번호" />
-          <br />
-          <button>로그인</button>
-          <br />
-          <button>회원가입</button>
+      <div className={styles.container}>
+        <img src={staticImagePath.timepaperLogo} className={styles.logoImage} />
+        <form action="" className={styles.formContainer}>
+          <input type="text" placeholder="아이디(이메일)" className={styles.inputId} />
+          <input type="text" placeholder="비밀번호" className={styles.inputPassword} />
+          <button className={styles.loginButton}>로그인</button>
+          <button className={styles.signupButton}>회원가입</button>
         </form>
       </div>
     </>
