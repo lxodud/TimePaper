8e5113a57939a50ed06c01d15d0a74ad8711af0f
yejin <yejinoh58@gmail.com>
2025-02-24 18:08:46 +0900

    [SSD-65] feat: 에러 페이지 구현
    - 404 Not Found 에러 페이지 추가
    - 홈으로 이동하는 링크 적용

--- frontend/src/pages/error/NotFound.jsx
+++ frontend/src/pages/error/NotFound.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import images from '../constant/staticImagePath';
+import images from '../../constant/staticImagePath';
 import { Link } from 'react-router-dom';
-import '../css/index.css';
+import styles from './NotFound.module.css';
 
 export default function NotFound() {
   return (
     <>
-      <div className="image-container">
+      <div className={styles.imageContainer}>
         <img src={images.notFound} alt="404 이미지" />
       </div>
-      <div className="home-link">
+      <div className={styles.homeLink}>
         <Link to="/">홈으로 이동</Link>
       </div>
     </>
