e73300cbf361e2a24eaaef3731ab082ca6c84a69
daejungyoon <dbseowjd12@gmail.com>
2025-02-24 18:39:23 +0900

    [SSD-67] refactor: CreateButton 컴포넌트, CSS 폴더 이동
    본문
    - 다른 페이지에서 쓰지 않는 한 페이지와 관련된 파일을 한 곳으로 모으기
      위해
    - 기존의 px 단위로 되어 있던 것 들을 %단위로 변경
    
    --- 커밋 타입 ---
    refactor: 코드 리팩토링

---
+++ frontend/src/pages/timepapercreate/CreateButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './CreateButton.module.css';
+export default function Button({ children }) {
+  return (
+    <button className={`${style.buttonBasic}`}>
+      {children}
+    </button>
+  );
+}
