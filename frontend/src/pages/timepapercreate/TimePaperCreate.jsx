4e94b3d2b40f77892ad58cf2a26697ee8cf6c398
daejungyoon <dbseowjd12@gmail.com>
2025-02-24 18:51:57 +0900

    [SSD-67] refactor: 파일 이동에 따른 import 경로 변경
    - placeholder 내용 변경 및 import 경로 변경

--- frontend/src/pages/timepapercreate/TimePaperCreate.jsx
+++ frontend/src/pages/timepapercreate/TimePaperCreate.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import Button from '../../components/createbutton/CreateButton';
-import CreateInput from '../../components/createinput/CreateInput';
+import Button from './CreateButton';
+import CreateInput from './CreateInput';
 import style from './timePaperCreate.module.css';
 
 export default function TimePaperCreate() {
   return (
     <>
       <div className={`${style.inputParentContainer}`}>
-        <CreateInput placeholder="롤링페이퍼 제목을 입력해주세요," />
+        <CreateInput placeholder="제목을 입력해주세요," />
       </div>
       <div className={`${style.buttonParentContainer}`}>
         <Button>타임페이퍼 생성</Button>
