60e46441537ae5757bc2eed6d58dd4d8065b73f3
lxodud <xodud1998223@naver.com>
2025-02-24 20:12:24 +0900

    [SSD-73] chore: underBarButton css 구현

--- frontend/src/components/BottomButton/BottomButton.jsx
+++ frontend/src/components/BottomButton/BottomButton.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import styles from './BottomButton.module.css'
+import React from 'react';
+import styles from './BottomButton.module.css';
 
 export default function BottomButton({ title, onClick }) {
   return (
-    <button onClick={onClick} className={styles.bottomButton}>{title}</button>
-  )
+    <button onClick={onClick} className={styles.bottomButton}>
+      {title}
+    </button>
+  );
 }
--- frontend/src/components/UnderBarButton/UnderBarButton.jsx
+++ frontend/src/components/UnderBarButton/UnderBarButton.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import styles from './UnderBarButton.module.css';
 
 export default function UnderBarButton({ title, onClick }) {
-  return <button onClick={onClick}>{title}</button>;
+  return (
+    <button onClick={onClick} className={styles.underBarButton}>
+      {title}
+    </button>
+  );
 }
