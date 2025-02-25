459e90c052b4859d65e04660ad6fb877a894e1a9
daejungyoon <dbseowjd12@gmail.com>
2025-02-25 13:12:07 +0900

    [SSD-67] chore: 생성페이지에서 사용하는 컴포넌트 명 변경
    - UnderBarButton 으로 사용되던 것을 UnderBarInput으로 변경

--- frontend/src/pages/timepapercreate/TimePaperCreate.jsx
+++ frontend/src/pages/timepapercreate/TimePaperCreate.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import styles from './timePaperCreate.module.css';
 import BottomButton from '../../components/BottomButton/BottomButton';
-import UnderBarButton from '../../components/UnderBarInput/UnderBarInput';
+import UnderBarInput from '../../components/UnderBarInput/UnderBarInput';
 
 export default function TimePaperCreate() {
   return (
     <>
       <div className={styles.container}>
-        <UnderBarButton placeholder={'제목을 입력해주세요'}></UnderBarButton>
+        <UnderBarInput placeholder={'제목을 입력해주세요'}></UnderBarInput>
         <BottomButton title={'타임페이퍼 생성'}></BottomButton>
       </div>
     </>
