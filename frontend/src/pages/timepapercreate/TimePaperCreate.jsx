cdaaae1a2e256317e8dbe3695ceb0c038318f2dc
daejungyoon <dbseowjd12@gmail.com>
2025-02-25 11:04:50 +0900

    [SSD-67] refactor: 공통 컴포넌트 적용
    - 기존의 개인 버튼, 인풋창  컴포넌트에서 공통 컴포넌트로 변경

--- frontend/src/pages/timepapercreate/TimePaperCreate.jsx
+++ frontend/src/pages/timepapercreate/TimePaperCreate.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import Button from './CreateButton';
-import CreateInput from './CreateInput';
 import styles from './timePaperCreate.module.css';
+import BottomButton from '../../components/BottomButton/BottomButton';
+import UnderBarButton from '../../components/UnderBarInput/UnderBarInput';
 
 export default function TimePaperCreate() {
   return (
     <>
       <div className={styles.container}>
-        <CreateInput placeholder="제목을 입력해주세요," />
-        <Button>타임페이퍼 생성</Button>
+        <UnderBarButton placeholder={'제목을 입력해주세요'}></UnderBarButton>
+        <BottomButton title={'타임페이퍼 생성'}></BottomButton>
       </div>
     </>
   );
