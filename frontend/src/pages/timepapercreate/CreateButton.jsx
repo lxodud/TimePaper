f8c5fe1a123af92365bfbdd191748b3dfffe4ffb
daejungyoon <dbseowjd12@gmail.com>
2025-02-25 13:14:55 +0900

    [SSD-67] chore: 불필요한 파일 제거
    - 사용하지 않는 CreateInput, CreateButton.jsx 제거

--- frontend/src/pages/timepapercreate/CreateButton.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './timePaperCreate.module.css';
-
-export default function Button({ children }) {
-  return <button className={styles.buttonBasic}>{children}</button>;
-}
