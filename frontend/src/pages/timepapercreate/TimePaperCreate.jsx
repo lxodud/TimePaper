49abe8a5127ddf8a1f30145c51309412b82fa33c
daejungyoon <dbseowjd12@gmail.com>
2025-02-25 13:22:43 +0900

    [SSD-67] chore: TimePaperCreate 페이지 input 컨테이너 생성
    - 원활한 width 조절을 위한 input 컨테이너 생성

--- frontend/src/pages/timepapercreate/TimePaperCreate.jsx
+++ frontend/src/pages/timepapercreate/TimePaperCreate.jsx
@@ -7,7 +7,9 @@ export default function TimePaperCreate() {
   return (
     <>
       <div className={styles.container}>
-        <UnderBarInput placeholder={'제목을 입력해주세요'}></UnderBarInput>
+        <div className={styles.inputContainer}>
+          <UnderBarInput placeholder={'제목을 입력해주세요'}></UnderBarInput>
+        </div>
         <BottomButton title={'타임페이퍼 생성'}></BottomButton>
       </div>
     </>
