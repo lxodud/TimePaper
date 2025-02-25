c3a995aa294988ce46200f4338cc02675952d67f
kdh <kdh462601@gmail.com>
2025-02-25 10:34:08 +0900

    [SSD-63] docs: 회원가입 페이지 UI 수정 중 branch siwtch를 위한 저장

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -2,22 +2,20 @@ import React, { useState } from 'react';
 import styles from './SignUp.module.css';
 
 export default function SignUp() {
-  const [isPwVisible, setIsPwVisible] = useState(false); // 비밀번호 조건 메시지의 가시성 상태
-  const [isPwCheckVisible, setisPwCheckVisible] = useState(false); // 비밀번호 조건 메시지의 가시성 상태
-  const [placeholdersVisible, setPlaceholdersVisible] = useState([true, true, true]); // 각 input의 placeholder 가시성 상태
-  const [isPrivacyPolicyAccepted, setIsPrivacyPolicyAccepted] = useState(false);
-  const [isTermsAccepted, setIsTermsAccepted] = useState(false);
+  const [isPwVisible, setIsPwVisible] = useState(false);
+  const [isPwCheckVisible, setisPwCheckVisible] = useState(false);
+  const [placeholdersVisible, setPlaceholdersVisible] = useState([true, true, true]);
 
   const handleFocus = (index) => {
     const newPlaceholdersVisible = [...placeholdersVisible];
-    newPlaceholdersVisible[index] = false; // 해당 input의 placeholder 숨기기
+    newPlaceholdersVisible[index] = false;
     setPlaceholdersVisible(newPlaceholdersVisible);
   };
 
   const handleBlur = (index, value) => {
     const newPlaceholdersVisible = [...placeholdersVisible];
     if (value === '') {
-      newPlaceholdersVisible[index] = true; // 내용이 없으면 해당 input의 placeholder 보이기
+      newPlaceholdersVisible[index] = true;
     }
     setPlaceholdersVisible(newPlaceholdersVisible);
   };
@@ -27,7 +25,7 @@ export default function SignUp() {
       <div className={styles.signMainBox}>
         <div className={`${styles.fieldsBox} ${styles.bigbox}`}>
           <div className={styles.contentBox}>
-            <div className={styles.fields}>이메일* </div>
+            <div className={styles.fields}>이메일*</div>
             <div className={styles.emailCheckBox}>
               <input
                 className={`${styles.fieldsInput} ${styles.borderRadius}`}
@@ -35,11 +33,10 @@ export default function SignUp() {
                 placeholder={placeholdersVisible[0] ? '이메일 작성' : ''}
                 onFocus={() => handleFocus(0)}
                 onBlur={(e) => handleBlur(0, e.target.value)}
-                style={{ textAlign: placeholdersVisible[0] ? 'center' : 'left' }} // placeholder가 보일 때 가운데 정렬
+                style={{ textAlign: placeholdersVisible[0] ? 'center' : 'left' }}
               />
               <button className={`${styles.emailCheckBut} ${styles.borderRadius}`}>인증</button>
             </div>
-
             <div className={styles.fields}>비밀번호*</div>
             <input
               className={`${styles.fieldsInput} ${styles.borderRadius}`}
@@ -47,7 +44,7 @@ export default function SignUp() {
               placeholder={placeholdersVisible[1] ? '비밀번호 작성' : ''}
               onFocus={() => handleFocus(1)}
               onBlur={(e) => handleBlur(1, e.target.value)}
-              style={{ textAlign: placeholdersVisible[1] ? 'center' : 'left' }} // placeholder가 보일 때 가운데 정렬
+              style={{ textAlign: placeholdersVisible[1] ? 'center' : 'left' }}
             />
             {isPwVisible && (
               <div className={`${styles.pwfont} ${styles.pw}`}>
@@ -62,7 +59,7 @@ export default function SignUp() {
               placeholder={placeholdersVisible[2] ? '비밀번호 확인' : ''}
               onFocus={() => handleFocus(2)}
               onBlur={(e) => handleBlur(2, e.target.value)}
-              style={{ textAlign: placeholdersVisible[2] ? 'center' : 'left' }} // placeholder가 보일 때 가운데 정렬
+              style={{ textAlign: placeholdersVisible[2] ? 'center' : 'left' }}
             />
             {isPwCheckVisible && (
               <div className={`${styles.pwfont} ${styles.pwcheck}`}>
