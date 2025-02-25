c15efcd367c1ed67829ed06648d7377a589cc49e
lxodud <xodud1998223@naver.com>
2025-02-25 22:00:17 +0900

    [SSD-56] feat: Home 페이지 css 구현

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import staticImagePath from '../../constant/staticImagePath';
 import BottomButton from '../../components/BottomButton/BottomButton';
+import styles from './Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <div>
-        <img src={staticImagePath.timepaperLogo} />
-        <BottomButton></BottomButton>
+      <div className={styles.container}>
+        <img src={staticImagePath.timepaperLogo} className='logo-image'/>
+        <div className={styles.buttonContainer}>
+          <BottomButton title={"롤링페이퍼 생성 하러가기"}></BottomButton>
+        </div>
       </div>
     </>
   );
