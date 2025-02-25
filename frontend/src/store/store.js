787e4a205665377832b2333766d9a1dc67f7e38d
Choi Wonnho <wonnho@nate.com>
2025-02-25 18:18:33 +0900

    [SSD-43]  chore: "write timepaperSlice.js under slices under store fo… (#18)
    * [SSD-43]  chore: "write timepaperSlice.js under slices under store folder and store.js under store folder"
    
    * [SSD-43] chore: update timepaperSlice.js and store.js
    
    코드가 다 올라가지 않아 나머지 부분을 추가함
    
    * [SSD-43]  chore: authSlice를 작성 후 store.js에 추가
    
    authSlice를 commit 후 store에 등록하지 않아 등록함함
    
    * [SSD-43] chore: delete timepaperSlice.jsx

---
+++ frontend/src/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import timepapersReducer from "./timepaperSlice";
+import authSlice from "./slices/authSlice";
+
+const store = configureStore({
+  reducer: {
+    timepapers: timepapersReducer,
+    auth:authReducer,    
+  },
+});
+
+export default store;
