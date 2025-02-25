cc3fd1d56be7fcb6bd49bf23e364127f3067619d
daejungyoon <dbseowjd12@gmail.com>
2025-02-25 15:55:50 +0900

    [SSD-67] feat: UnderBarInput value 요소 추가
    - input value 요소 추가

--- frontend/src/components/UnderBarInput/UnderBarInput.jsx
+++ frontend/src/components/UnderBarInput/UnderBarInput.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './UnderBarInput.module.css';
 
-export default function UnderBarInput({ placeholder, onChange }) {
+export default function UnderBarInput({ value, placeholder, onChange }) {
   return (
     <input
       type="text"
+      value={value}
       className={styles.underBarInput}
       placeholder={placeholder}
       onChange={onChange}
