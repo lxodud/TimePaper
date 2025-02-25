16b9afd5b237a997900d0575f6210936369cbdf7
lxodud <xodud1998223@naver.com>
2025-02-25 11:11:44 +0900

    [SSD-62] refactor: 전역 css 및 공통 컴포넌트 적용

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import staticImagePath from '../../constant/staticImagePath';
-import styles from './Login.module.css'
+import styles from './Login.module.css';
+import BottomButton from '../../components/BottomButton/BottomButton';
 
 export default function Login() {
   return (
     <>
       <div className={styles.container}>
-        <img src={staticImagePath.timepaperLogo} className={styles.logoImage} />
+        <img src={staticImagePath.timepaperLogo} className='logo-image' />
         <form action="" className={styles.formContainer}>
           <input type="text" placeholder="아이디(이메일)" className={styles.inputId} />
           <input type="text" placeholder="비밀번호" className={styles.inputPassword} />
-          <button className={styles.loginButton}>로그인</button>
-          <button className={styles.signupButton}>회원가입</button>
+          <div className={styles.buttonContainer}>
+            <BottomButton title={'로그인'}></BottomButton>
+            <button className={styles.signupButton}>회원가입</button>
+          </div>
+          
         </form>
       </div>
     </>
