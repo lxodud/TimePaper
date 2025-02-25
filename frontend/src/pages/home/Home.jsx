519fffd9add0eae3d4c61c2316f32d60f386ffa8
lxodud <xodud1998223@naver.com>
2025-02-25 22:14:07 +0900

    [SSD-56] feat: Home 페이지 로직 구현

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import staticImagePath from '../../constant/staticImagePath';
 import BottomButton from '../../components/BottomButton/BottomButton';
-import styles from './Home.module.css'
+import styles from './Home.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../../store/slices/authSlice';
 
 export default function Home() {
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  const handleToCreateButtonClick = () => {
+    if (isLoggedIn) {
+      navigate('/timepaper/create');
+    } else {
+      navigate('/login');
+    }
+  };
+
   return (
     <>
       <div className={styles.container}>
-        <img src={staticImagePath.timepaperLogo} className='logo-image'/>
+        <img src={staticImagePath.timepaperLogo} className="logo-image" />
         <div className={styles.buttonContainer}>
-          <BottomButton title={"롤링페이퍼 생성 하러가기"}></BottomButton>
+          <BottomButton
+            title={'롤링페이퍼 생성 하러가기'}
+            onClick={handleToCreateButtonClick}
+          ></BottomButton>
         </div>
       </div>
     </>
