c5bb774640492fc70f5a3b3344fd204c6d4b0fa7
yejin <yejinoh58@gmail.com>
2025-02-26 14:53:59 +0900

    [SSD-83] chore: 개발용 accessToken 유효기간 7일로 변경 (#23)
    * [SSD-83] chore: 개발용 accessToken 유효기간 7일로 변경
    
    * [SSD-83] chore: 개발 시 데이터 유지가 가능하도록 Hibernate DDL 설정 변경

--- backend/src/main/java/com/timepaper/backend/global/auth/jwt/util/JWTUtil.java
+++ backend/src/main/java/com/timepaper/backend/global/auth/jwt/util/JWTUtil.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Component;
 public class JWTUtil {
 
   private static final Logger log = LoggerFactory.getLogger(JWTUtil.class);
-  private final long tokenValidityInMilliseconds = 1000L * 60 * 30; // 30분
+  private final long tokenValidityInMilliseconds = 1000L * 60 * 60 * 24 * 7; // 개발용 7일
 
   @Value("${jwt.secret}")
   private String secretKey;
