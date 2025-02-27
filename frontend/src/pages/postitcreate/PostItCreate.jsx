88ab34696d6b0be7fde35d11294b1cd03e500fe3
yejin <yejinoh58@gmail.com>
2025-02-27 21:22:42 +0900

    [SSD-91] feat: 포스트잇 생성 페이지 구현

---
+++ frontend/src/pages/postitcreate/PostItCreate.jsx
@@ -0,0 +1,85 @@
+import React, { useRef, useState } from 'react';
+import staticImagePath from '../../constant/staticImagePath';
+import styles from './PostItCreate.module.css';
+import UnderBarInput from '../../components/UnderBarInput/UnderBarInput';
+import BottomButton from '../../components/BottomButton/BottomButton';
+
+export default function PostItCreate() {
+  const fileInputRef = useRef(null);
+  const [staticImage, setStaticImage] = useState(staticImagePath.postitAfternoon);
+  const [customImage, setCustomImage] = useState(null);
+
+  const templates = [staticImagePath.postitAfternoon, staticImagePath.postitNight];
+
+  const handleImageUpload = (e) => {
+    console.log('이미지 업로드 처리 클릭');
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setCustomImage(event.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleTemplateSelect = (src) => {
+    setStaticImage(src);
+    setCustomImage(null);
+  };
+
+  const handleImageSelectClick = () => {
+    console.log('이미지 업로드 아이콘 클릭');
+    console.log(fileInputRef.current);
+    fileInputRef.current.click();
+  };
+
+  const handleSaveBtn = (e) => {
+    e.preventDefault();
+    console.log('등록 선택');
+  };
+
+  return (
+    <>
+      <form className={styles.container}>
+        <div className={styles.underBarInput}>
+          <UnderBarInput placeholder="작성자 입력"></UnderBarInput>
+        </div>
+        <div className={styles.selectedImage}>
+          <img
+            src={customImage || staticImage}
+            className="logo-image"
+            alt="선택된 포스트잇 이미지"
+          />
+          <textarea className={styles.textarea}></textarea>
+        </div>
+        <section className={styles.imageContainer}>
+          {templates.map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              className={styles.templateThumbnail}
+              onClick={() => handleTemplateSelect(src)}
+              alt={`테마 ${index + 1}`}
+            />
+          ))}
+          <img
+            src={staticImagePath.imageSelectIcon}
+            className={styles.templateThumbnail}
+            onClick={handleImageSelectClick}
+            alt="사용자가 선택한 이미지"
+          />
+          <input
+            type="file"
+            ref={fileInputRef}
+            className={styles.fileInput}
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+        </section>
+        <BottomButton title="생성" onClick={handleSaveBtn}></BottomButton>
+      </form>
+    </>
+  );
+}
