0a11c47faae1b6ea942dc68049c3dea19f80067b
Choi Wonnho <wonnho@nate.com>
2025-02-27 20:30:50 +0900

    [SSD-76] fix: repository annotation 제거

--- backend/src/main/java/com/timepaper/backend/domain/my/repository/MyTimepaperRepository.java
+++ backend/src/main/java/com/timepaper/backend/domain/my/repository/MyTimepaperRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface MyTimepaperRepository extends JpaRepository<TimePaper, UUID> {
   // 특정 사용자의 TimePaper 조회
   List<TimePaper> findByCreatorId(Long creatorId);
