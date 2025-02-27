bb44ae56e36863c75bc9e586d358b753c2b67ee3
DaeJungYoon <dbseowjd12@gmail.com>
2025-02-27 11:32:23 +0900

    [SSD-81] 타임페이퍼 조회 기능 구현 (#29)
    * [SSD-81] feat: 타임페이퍼 조회 기능 구현
    
    - 특정 타임페이퍼의 정보가 들어있는
    데이터를 조회할 수 있는 기능 구현
    
    * [SSD-81] chore: 임시파일 제거
    
    - 물결 표시의 임시 파일 제거

--- backend/src/main/java/com/timepaper/backend/domain/timepaper/controller/TimePaperController.java
+++ backend/src/main/java/com/timepaper/backend/domain/timepaper/controller/TimePaperController.java
@@ -5,11 +5,14 @@ import com.timepaper.backend.domain.timepaper.dto.response.TimePaperResponseDto;
 import com.timepaper.backend.domain.timepaper.service.TimePaperService;
 import com.timepaper.backend.global.dto.ApiResponse;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +37,16 @@ public class TimePaperController {
                 "SUCCESS",
                 responseDto));
   }
+
+  @GetMapping("/{timepaperId}")
+  public ResponseEntity<ApiResponse<TimePaperResponseDto>> readTimePaperById(@PathVariable UUID timepaperId) {
+
+    TimePaperResponseDto responseDto = timePaperService.readTimePaperById(timepaperId);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(ApiResponse
+            .ok("타임페이퍼 조회 성공",
+                "OK",
+                responseDto));
+  }
+
 }
