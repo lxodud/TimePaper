4b94b5ceebd17753a238f3ce05069a998c78515d
lxodud <xodud1998223@naver.com>
2025-02-27 15:48:26 +0900

    [SSD-88] feat: BottomButton enable 컬러 수정

--- frontend/src/components/BottomButton/BottomButton.jsx
+++ frontend/src/components/BottomButton/BottomButton.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './BottomButton.module.css';
 
-export default function BottomButton({ title, onClick }) {
+export default function BottomButton({ title, onClick, isEnable }) {
+  const [buttonColor, setButtonColor] = useState({ enableColor: '', disableColor: '' });
+
+  useEffect(() => {
+    const rootStyle = getComputedStyle(document.documentElement);
+    const enableColor = rootStyle.getPropertyValue('--orange-color');
+    const disableColor = rootStyle.getPropertyValue('--disable-gray-color');
+
+    setButtonColor({
+      enableColor: enableColor,
+      disableColor: disableColor,
+    });
+  }, []);
+
   return (
-    <button onClick={onClick} className={styles.bottomButton}>
+    <button
+      onClick={onClick}
+      className={styles.bottomButton}
+      disabled={isEnable}
+      style={{ backgroundColor: isEnable ? buttonColor.enableColor : buttonColor.disableColor }}
+    >
       {title}
     </button>
   );
