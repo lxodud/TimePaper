8b326ed54c4f913bb992ddb0efd0a7354b0ddbb1
yejin <yejinoh58@gmail.com>
2025-03-01 01:43:36 +0900

    [SSD-102] feat: 공통 컴포넌트 UnderBarInput에 name 속성 및 props 추가

--- frontend/src/components/UnderBarInput/UnderBarInput.jsx
+++ frontend/src/components/UnderBarInput/UnderBarInput.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './UnderBarInput.module.css';
 
-export default function UnderBarInput({ value, placeholder, onChange }) {
+export default function UnderBarInput({ value, placeholder, onChange, name }) {
   return (
     <input
       type="text"
+      name={name}
       value={value}
       className={styles.underBarInput}
       placeholder={placeholder}
