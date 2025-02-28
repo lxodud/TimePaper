5f3aa25b6f8ea63dfafc8a0e89685d759f48d78b
daejungyoon <dbseowjd12@gmail.com>
2025-02-28 10:37:04 +0900

    [SSD-93] fix: 폴더 명 변경으로 인한 라우터 설정 변경
    - 타임페이퍼 캡슐 페이지 폴더 명 변경으로 인한 라우터 변경

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -10,7 +10,7 @@ import PostItCreate from '../pages/PostItCreate';
 import NotFound from '../pages/error/NotFound';
 import HeaderLayout from '../layout/HeaderLayout.jsx';
 import TimePaperCreate from '../pages/timepapercreate/TimePaperCreate.jsx';
-import TimePaperSetLock from '../pages/timepaperislocked/TimePaperSetLock.jsx';
+import TimePaperSetLock from '../pages/timepaperSetlocked/TimePaperSetLock.jsx';
 
 const router = createBrowserRouter([
   {
