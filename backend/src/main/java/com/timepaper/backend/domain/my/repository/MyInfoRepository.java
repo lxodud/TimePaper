1775fa3f48c7aaf0f4c364b0e6853a7c1db44d15
yejin <yejinoh58@gmail.com>
2025-03-01 00:49:52 +0900

    [SSD-107] 마이 정보 관련 API (#44)
    * [SSD-107] refactor: 마이 정보 관련 API
    
    - 기존 Repository 사용하도록 리팩터링
    - GET: /api/my/myinfo -> /api/my
    
    * [SSD-107] fix: 메서드명 수정

--- backend/src/main/java/com/timepaper/backend/domain/my/repository/MyInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.timepaper.backend.domain.my.repository;
-
-import com.timepaper.backend.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyInfoRepository extends JpaRepository<User, Long> {
-
-  User findByEmail(String email);
-}
