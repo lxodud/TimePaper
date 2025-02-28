64c37aa0b390038784c53a40eddf941755472159
daejungyoon <dbseowjd12@gmail.com>
2025-02-28 18:35:06 +0900

    [SSD-100] feat: TimePaperDetail 페이지 헤더 적용
    - 가짜 api 함수와 함께 헤더 적용

--- frontend/src/pages/timepaperDetail/TimePaperDetail.jsx
+++ frontend/src/pages/timepaperDetail/TimePaperDetail.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './timepaperdetail.module.css';
+import { useDispatch } from 'react-redux';
+import { setPageTitle } from '../../store/slices/headerSlice';
 
+const tempPostTimePaper = async (data) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ title: '2022년도 졸업식' });
+    }, 1000);
+  });
+};
 export default function TimePaperDetail() {
   const { timepaperId } = useParams();
-  const [timepapers, setTimepapers] = useState([
+  const [postIt, setPostIt] = useState([
     {
       id: 1,
       title: '감사합니다.',
@@ -56,13 +65,26 @@ export default function TimePaperDetail() {
       post: '/images/postitD.png',
     },
   ]);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const updateHeaderTitle = async () => {
+      try {
+        // 필요시 timepaperId 등 data를 전달합니다.
+        const data = await tempPostTimePaper({ timepaperId });
+        dispatch(setPageTitle(data.title)); // 전역 상태에 타이틀 업데이트
+      } catch (error) {
+        console.error('Error fetching timepaper data:', error);
+      }
+    };
+    updateHeaderTitle();
+  }, [timepaperId, dispatch]);
   return (
     <div className={styles.container}>
       <h2>2022년도 졸업식</h2>
 
       <ul className={styles.timepaperList}>
-        {timepapers.map((paper) => (
+        {postIt.map((paper) => (
           <li key={paper.id} className={styles.timepaperItem}>
             <h3>{paper.title}</h3>
             {/* 이미지 경로를 src 속성에 전달 */}
