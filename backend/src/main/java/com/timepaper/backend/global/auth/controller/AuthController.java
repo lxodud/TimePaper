8440ac0a62355376b3cde883281e3ccfa5297030
yejin <yejinoh58@gmail.com>
2025-03-02 16:51:26 +0900

    [SSD-110] 로그아웃 api 구현 (#50)
    * [SSD-110] feat: 로그아웃 API 요청시 refreshToken 쿠키 만료처리, Redis refreshToken 삭제
    
    * [SSD-110] feat: 로그아웃시 쿠키 삭제 경로 변경
    
    - refreshToken이 만료되거나 오염된 경우 예외 처리
    - 로그아웃 시 JWT를 사용하는 방식으로 리팩토링
    - 로그아웃 시 쿠키 경로를 '/'로 변경 -> 전체 경로에서 쿠키 만료 적용
    
    * [SSD-110] feat: login, logout시 응답 쿠키 path 변경
    
    - /api/auth로 통일
    
    * [SSD-110] chore: WIP 프론트 연동 for  리프레시 토큰 path 테스트

--- backend/src/main/java/com/timepaper/backend/global/auth/controller/AuthController.java
+++ backend/src/main/java/com/timepaper/backend/global/auth/controller/AuthController.java
@@ -1,10 +1,10 @@
 package com.timepaper.backend.global.auth.controller;
 
 import com.timepaper.backend.global.auth.service.AuthService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +19,19 @@ public class AuthController {
   private final AuthService authService;
 
   @PostMapping("/auth/reissue")
-  public void reissueToken(HttpServletRequest request,
-      HttpServletResponse response,
+  public void reissueToken(HttpServletResponse response,
       @CookieValue(value = "refresh_token", required = true) String refreshToken) {
-    request.getHeader("Cookie");
 
-    log.info("reissue 호출");
     log.info("refreshToken : {}", refreshToken);
     authService.reissueToken(response, refreshToken);
+  }
+
+  @PostMapping("/auth/logout")
+  public void logout(HttpServletResponse response,
+      @CookieValue(value = "refresh_token", required = false) String refreshToken,
+      Authentication authentication) {
+    log.info("refreshToken : {}", refreshToken);
+    authService.logout(response, refreshToken, authentication);
   }
 
 }
