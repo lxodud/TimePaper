bdda21604b2c48ec356f850b93660c2dade21fe9
yejin <yejinoh58@gmail.com>
2025-03-02 15:16:29 +0900

    [SSD-111] feat: 버튼 위치 조정

--- frontend/src/layout/RootLayout.jsx
+++ frontend/src/layout/RootLayout.jsx
@@ -4,6 +4,7 @@ import { logout } from '../store/slices/authSlice';
 import { Outlet } from 'react-router-dom';
 import UnderBarButton from '../components/UnderBarButton/UnderBarButton';
 import { api } from '../api/api';
+import styles from './RootLayout.module.css';
 
 export default function RootLayout() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
@@ -31,9 +32,13 @@ export default function RootLayout() {
     <>
       <Outlet></Outlet>
       {/* 개발 편의성을 위한 임시 로그아웃 버튼입니다. */}
-      <footer>
-        {isLoggedIn && <UnderBarButton onClick={handleLogout} title={'로그아웃'}></UnderBarButton>}
-      </footer>
+      <div className={styles.footerContainer}>
+        <footer>
+          {isLoggedIn && (
+            <UnderBarButton onClick={handleLogout} title={'로그아웃'}></UnderBarButton>
+          )}
+        </footer>
+      </div>
     </>
   );
 }
