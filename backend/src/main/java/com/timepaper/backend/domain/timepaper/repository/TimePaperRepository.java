2213f25869261369ed3e043c6277818cc730e00e
lxodud <xodud1998223@naver.com>
2025-03-02 23:34:30 +0900

    [SSD-115] feat: 현재 날짜와 발송 날짜가 동일한 TimePaper를 조회하는 메서드 구현

--- backend/src/main/java/com/timepaper/backend/domain/timepaper/repository/TimePaperRepository.java
+++ backend/src/main/java/com/timepaper/backend/domain/timepaper/repository/TimePaperRepository.java
@@ -5,8 +5,12 @@ import com.timepaper.backend.domain.user.entity.User;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TimePaperRepository extends JpaRepository<TimePaper, UUID> {
 
   List<TimePaper> findByCreator(User user);
+
+  @Query("SELECT t FROM TimePaper t WHERE DATE(t.releaseDate) = CURRENT_DATE")
+  List<TimePaper> findAllByReleaseDateToday();
 }
