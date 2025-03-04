9b3b5969ea68a2e25af5be13ac09bd9370262904
lxodud <xodud1998223@naver.com>
2025-03-04 17:26:25 +0900

    [SSD-115] feat: 타임페이퍼 스케쥴링 이메일 전송 로직 구현

--- backend/src/main/java/com/timepaper/backend/global/scheduler/EmailSendScheduler.java
+++ backend/src/main/java/com/timepaper/backend/global/scheduler/EmailSendScheduler.java
@@ -2,9 +2,11 @@ package com.timepaper.backend.global.scheduler;
 
 import com.timepaper.backend.domain.timepaper.entity.TimePaper;
 import com.timepaper.backend.domain.timepaper.repository.TimePaperRepository;
+import com.timepaper.backend.global.emailsender.EmailSendManager;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +16,22 @@ import org.springframework.stereotype.Component;
 public class EmailSendScheduler {
 
   private final TimePaperRepository timePaperRepository;
+  private final EmailSendManager emailSendManager;
+
+  @Value("${TIME_PAPER_URL}")
+  private String timePaperUrl;
 
   @Scheduled(cron = "0 0 0 * * ?")
   public void sendEmail() {
-    List<String> recipientEmails = timePaperRepository.findAllByReleaseDateToday()
-                                       .stream()
-                                       .map((TimePaper::getRecipientEmail))
-                                       .toList();
+    List<TimePaper> timepapers = timePaperRepository.findAllByReleaseDateToday()
+                                     .stream()
+                                     .toList();
 
     // TODO: 이메일 전송
+    timepapers.forEach(timePaper -> {
+      String url = timePaperUrl + String.valueOf(timePaper.getId());
+      emailSendManager.sendEmail(timePaper.getRecipientEmail(), "타임페이퍼 오픈", url);
+    });
+
   }
 }
