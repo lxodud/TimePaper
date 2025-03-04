55723f15889ae2aaf37c035aa7f577e81cb22097
yejin <yejinoh58@gmail.com>
2025-03-04 17:05:10 +0900

    [SSD-130] refactor: AuthRepository -> UserRepositry
    - 공통된 Repository 사용하도록 변경

--- backend/src/main/java/com/timepaper/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/timepaper/backend/domain/user/repository/UserRepository.java
@@ -3,9 +3,14 @@ package com.timepaper.backend.domain.user.repository;
 import com.timepaper.backend.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<UserDetails> findByEmail(String email);
+
+  @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.email = :email")
+  boolean isExistEmail(@Param("email") String email);
 }
