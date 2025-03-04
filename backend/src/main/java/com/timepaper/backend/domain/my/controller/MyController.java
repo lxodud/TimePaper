9dcb0946b0e9f5eb7bd4579a9f275e61365ca3ac
yejin <yejinoh58@gmail.com>
2025-03-04 11:24:56 +0900

    [SSD-127] refactor: DTO import 문 제거
    - 불필요한 API 삭제
    - GET: /api/my
    - 사용자 정보 로드 API -> JWT 활용 으로 변경

--- backend/src/main/java/com/timepaper/backend/domain/my/controller/MyController.java
+++ backend/src/main/java/com/timepaper/backend/domain/my/controller/MyController.java
@@ -1,6 +1,5 @@
 package com.timepaper.backend.domain.my.controller;
 
-import com.timepaper.backend.domain.my.dto.response.MyInfoResponseDto;
 import com.timepaper.backend.domain.my.dto.response.MyPostitListResponseDto;
 import com.timepaper.backend.domain.my.dto.response.MyTimepaperListResponseDto;
 import com.timepaper.backend.domain.my.service.MyService;
@@ -31,13 +30,6 @@ public class MyController {
   public ResponseEntity<ApiResponse<List<MyPostitListResponseDto>>> getMyPostits(
       @AuthenticationPrincipal User authenticatedUser) {
     return ResponseEntity.ok(ApiResponse.ok(myService.getMyPostits(authenticatedUser)));
-  }
-
-  @GetMapping()
-  public ResponseEntity<ApiResponse<MyInfoResponseDto>> getMyInfo(
-      @AuthenticationPrincipal User authenticatedUser) {
-    return ResponseEntity.ok(
-        ApiResponse.ok(myService.getMyInfo(authenticatedUser)));
   }
 
 }
