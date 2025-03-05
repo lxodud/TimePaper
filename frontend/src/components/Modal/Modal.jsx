8551d7dd2f8572e0d8760ea0e9f1e5b5ef6c0c5c
daejungyoon <dbseowjd12@gmail.com>
2025-03-05 13:51:12 +0900

    [SSD-128] chore: modal 수정
    - 구조 및 css 수정

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import styles from './Modal.module.css';
+import Dropdown from '../dropdownmenu/Dropdown.jsx';
 
-function Modal({ onClose, imageUrl, modalContent, from }) {
+function Modal({ onClose, onDelete, imageUrl, modalContent, from }) {
+  const handleSelect = (value) => {
+    if (value === 'delete') {
+      onDelete(); // 포스트잇 삭제 동작 실행
+    } else if (value === 'close') {
+      onClose(); // 모달 닫기 동작 실행
+    }
+  };
+
   return (
     <div className={styles.overlay} onClick={onClose}>
       <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-        <button onClick={onClose} className={styles.closeButton}>
-          &times;
-        </button>
+        {/* Dropdown 컴포넌트에 options와 onSelect 전달 */}
         <div className={styles.selectedImage}>
+          <Dropdown
+            options={[
+              { label: '포스트잇 삭제', value: 'delete' },
+              { label: '닫기', value: 'close' },
+            ]}
+            onSelect={handleSelect}
+          />
           <img src={imageUrl} className={styles.logoImage} alt="선택된 포스트잇 이미지" />
           <textarea className={styles.textareaWrapper} readOnly>
             {modalContent}
