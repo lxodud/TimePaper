ba91716cdb09b67e9685f4bafa5ff6d3e09a7586
Hamo <85005933+lxodud@users.noreply.github.com>
2025-03-05 14:03:09 +0900

    [SSD-120] loading component 구현 (#73)
    * [SSD-120] feat: loading 상태 Redux 구현
    
    * [SSD-120] feat: Loading 컴포넌트 구현
    
    * [SSD-120] fix: isLoading state select 오류 수정
    
    * [SSD-120] feat: 회원가입 예외처리
    
    * [SSD-120] feat: ErrorAlert 구현
    
    * [SSD-120] style: 주석 제거
    
    * [SSD-120] chore: Alert로 네이밍 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,35 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './css/index.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { login, loadUser } from './store/slices/authSlice';
+import { login } from './store/slices/authSlice';
 import { api } from './api/api';
-
+import Loading from './components/Loading/Loading';
+import { finishLoading, startLoading } from './store/slices/loadingSlice';
 
 export default function App() {
   const dispatch = useDispatch();
-  const { accessToken } = useSelector((state) => state.auth);
+  const isLoading = useSelector((state) => state.loading.isLoading);
 
   useEffect(() => {
     (async () => {
+      dispatch(startLoading())  
+      try {
       const response = await api.reissue();
       dispatch(login(response.headers.authorization));
+      } catch {
+
+      } finally {
+        dispatch(finishLoading())
+      }
+      
     })();
+
   }, []);
 
   return (
     <>
       <div className="main-container">
+        {isLoading && <Loading></Loading>}
         <RouterProvider router={router}></RouterProvider>
       </div>
     </>
