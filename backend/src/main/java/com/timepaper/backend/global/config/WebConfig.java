68ae3c7373992fc2a84efe59c5990eeb996bdf36
yejin <yejinoh58@gmail.com>
2025-03-06 16:42:24 +0900

    [SSD-156] refactor: 환경변수 파일 정리

--- backend/src/main/java/com/timepaper/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/timepaper/backend/global/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173", origin) //임시 배포시 변경
+        .allowedOrigins(origin)
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         .allowedHeaders("*")
         .allowCredentials(true)
